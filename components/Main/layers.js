df922fe9e7866899cbc24addccb77c0a7754aae1
zverev <alexander.zverev@gmail.com>
2015-09-28 13:30:43 +0300

    use layers section to configure layers

--- components/Main/layers.js
+++ components/Main/layers.js
@@ -10,55 +10,12 @@ cm.define('layersTree', ['gmxApplication'], function(cm) {
     return cm.get('gmxApplication').get('layersTree');
 });
 
-cm.define('layersTranslations', ['config', 'layersTree'], function(cm) {
-    var config = cm.get('config');
-    var layersTree = cm.get('layersTree');
-
-    if (!config.user.layersTranslations || nsGmx.Translations.getLanguage() !== 'eng') {
-        return null;
-    }
-
-    for (var groupId in config.user.layersTranslations) {
-        var layer = layersTree.find(groupId);
-        if (layer) {
-            var p = layer.get('properties');
-            p.title = config.user.layersTranslations[groupId];
-        }
-    }
-
-    return true;
-});
-
-cm.define('sectionsManager', ['config', 'layersTree', 'layersTranslations'], function() {
+cm.define('sectionsManager', ['config', 'layersTree'], function() {
     var config = cm.get('config');
     var layersTree = cm.get('layersTree');
     return new nsGmx.SectionsManager({
         sectionsTree: layersTree.find(config.user.sectionsTree)
     });
-});
-
-cm.define('newsLayersClusters', ['layersHash', 'sectionsManager', 'map'], function(cm) {
-    var map = cm.get('map');
-    var layersHash = cm.get('layersHash');
-    var sectionsManager = cm.get('sectionsManager');
-    sectionsManager.getSectionsIds().map(function(sectionId) {
-        var layer = layersHash[sectionsManager.getSectionProperties(sectionId).dataLayerId];
-        layer && layer.bindClusters({
-            maxZoom: map.getMaxZoom(),
-            zoomToBoundsOnClick: false,
-            clusterclick: function(e) {
-                var bounds = e.layer.getBounds();
-                var nw = bounds.getNorthWest();
-                var se = bounds.getSouthEast();
-                if (nw.distanceTo(se) === 0) {
-                    e.layer.spiderfy();
-                } else {
-                    e.layer.zoomToBounds();
-                }
-            }
-        });
-    });
-    return null;
 });
 
 cm.define('newsLayersCollections', ['sectionsManager', 'layersHash', 'calendar'], function(cm) {
