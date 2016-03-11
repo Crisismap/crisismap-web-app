863b2c55a3a117fb64f038000a3b76051b0e481a
zverev <alexander.zverev@gmail.com>
2016-03-11 13:57:51 +0300

    filter alerts by class

--- components/AlertsWidget/AlertsWidget.js
+++ components/AlertsWidget/AlertsWidget.js
@@ -1,6 +1,10 @@
 window.nsGmx = window.nsGmx || {};
 
 window.nsGmx.AlertsWidget = nsGmx.CompositeScrollView.extend({
+    options: {
+        field: 'class'
+    },
+
     initialize: function(options) {
 
         this.alertsListWidget = new nsGmx.SwitchingCollectionWidget({
@@ -9,15 +13,25 @@ window.nsGmx.AlertsWidget = nsGmx.CompositeScrollView.extend({
         });
 
         this.collectionFilterWidget = new nsGmx.CollectionFilterWidget({
-            field: 'class'
+            field: this.options.field
         });
+
+        this._activeValues = this.collectionFilterWidget.getActiveValues();
+
+        this.collectionFilterWidget.on('change', function(activeValues) {
+            this._activeValues = activeValues;
+            this._updateAlertsListWidget();
+        }.bind(this));
+
+        // save old options
+        var opts = this.options;
 
         nsGmx.CompositeScrollView.prototype.initialize.call(this, {
             staticWidget: this.collectionFilterWidget,
             scrollingWidget: this.alertsListWidget
         });
 
-        this.options = _.extend(this.options, options);
+        this.options = _.extend(opts, this.options, options);
 
         this.options.sectionsManager.on('sectionchange', this._onSectionChange, this);
 
@@ -30,30 +44,41 @@ window.nsGmx.AlertsWidget = nsGmx.CompositeScrollView.extend({
         }
     },
 
-
-    updateCollection: function() {
-        console.time('set_collection');
-        var collection = this.options.newsLayersCollections[this.options.sectionsManager.getActiveSectionId()];
-        if (!collection) {
-            return;
-        }
-        this.alertsListWidget.setCollection(collection);
-        this.collectionFilterWidget.setCollection(collection);
-        console.timeEnd('set_collection');
+    _getCollection: function () {
+        return this.options.newsLayersCollections[this.options.sectionsManager.getActiveSectionId()];
     },
 
-    _onSectionChange: function (sectionId) {
-        this.updateCollection();
+    _getFilteredCollection: function() {
+        var collection = this._getCollection();
+        if (!collection) {
+            return new Backbone.Collection();
+        }
+        return new Backbone.Collection(collection.filter(function(model) {
+            return this._activeValues.indexOf(model.get(this.options.field)) > -1;
+        }.bind(this)));
+    },
+
+    _updateAlertsListWidget: function() {
+        this.alertsListWidget.setCollection(this._getFilteredCollection());
+    },
+
+    _updateCollectionFilterWidget: function() {
+        this.collectionFilterWidget.setCollection(this._getCollection());
+    },
+
+    _onSectionChange: function(sectionId) {
+        // this.updateCollection();
     },
 
     _onSectionCollectionUpdate: function(collection) {
         var activeSectionId = this.options.sectionsManager.getActiveSectionId();
         if (this._getSectionIdByCollection(collection) === activeSectionId) {
-            console.log(activeSectionId);
+            this._updateCollectionFilterWidget();
+            this._updateAlertsListWidget();
         }
     },
 
-    _getSectionIdByCollection: function (collection) {
+    _getSectionIdByCollection: function(collection) {
         var id;
         Object.keys(this.options.newsLayersCollections).map(function(sectionId) {
             if (this.options.newsLayersCollections[sectionId] === collection) {
--- components/CollectionFilterWidget/CollectionFilterWidget.js
+++ components/CollectionFilterWidget/CollectionFilterWidget.js
@@ -3,16 +3,12 @@ window.nsGmx = window.nsGmx || {};
 window.nsGmx.CollectionFilterWidget = nsGmx.GmxWidget.extend({
     className: 'filterWidget',
 
-    events: {
-        'click': '_onClick'
-    },
-
     // options.collection
     // options.field
     initialize: function(options) {
         this.options = _.extend({}, options);
 
-        this._models = {};
+        this._models = [];
         this.options.collection && this.setCollection(this.options.collection)
         this.render();
     },
@@ -20,16 +16,15 @@ window.nsGmx.CollectionFilterWidget = nsGmx.GmxWidget.extend({
     render: function() {
         this.$el.empty();
 
-        Object.keys(this._models).map(function (value) {
+        this._models.map(function(model) {
             var w = new nsGmx.CheckboxWidget({
-                model: this._models[value],
-                label: value
+                model: model
             });
             w.appendTo(this.$el);
         }.bind(this));
     },
 
-    setCollection: function (collection) {
+    setCollection: function(collection) {
         if (this.collection) {
             this.collection.off('update', this._onCollectionUpdate, this);
         }
@@ -38,19 +33,47 @@ window.nsGmx.CollectionFilterWidget = nsGmx.GmxWidget.extend({
         this._onCollectionUpdate();
     },
 
-    _onCollectionUpdate: function () {
+    getActiveValues: function() {
+        return this._models.filter(function(model) {
+            return model.get('state');
+        }).map(function(model) {
+            return model.get('value');
+        });
+    },
+
+    _onCollectionUpdate: function() {
+        this._unbindModels();
         this._models = [];
-        this.collection.map(function (model) {
-            if (this._models.indexOf(model.get(this.options.field)) === -1) {
-                this._models[model.get(this.options.field)] = new Backbone.Model({
+        var modelsValues = [];
+        this.collection.map(function(model) {
+            if (modelsValues.indexOf(model.get(this.options.field)) === -1) {
+                this._models.push(new Backbone.Model({
+                    value: model.get(this.options.field),
                     state: true
+                }));
+                modelsValues = this._models.map(function(model) {
+                    return model.get('value');
                 });
             }
         }.bind(this));
+        this._bindModels();
+        this.trigger('change', this.getActiveValues());
         this.render();
     },
 
-    _onClick: function (je) {
-        console.log(je.target);
+    _bindModels: function() {
+        this._models.map(function(model) {
+            model.on('change', this._onModelChange, this);
+        }.bind(this));
+    },
+
+    _unbindModels: function() {
+        this._models.map(function(model) {
+            model.off('change', this._onModelChange, this);
+        }.bind(this));
+    },
+
+    _onModelChange: function() {
+        this.trigger('change', this.getActiveValues());
     }
 });
