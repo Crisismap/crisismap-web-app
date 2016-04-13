1bd1a7da6dc8df4b0618fd59db3227eccee6feb7
zverev <alexander.zverev@gmail.com>
2016-04-13 14:04:11 +0300

    geolocationMarker: add accuracy circle

--- components/Main/lib/LocationMarkerManager.js
+++ components/Main/lib/LocationMarkerManager.js
@@ -1,12 +1,33 @@
 var nsGmx = nsGmx || {};
 
-nsGmx.LocationMarker = L.Marker.extend({
+nsGmx.LocationMarker = L.Class.extend({
+    // options.circleRadius
+    initialize: function(options) {
+        L.setOptions(this, options);
+    },
 
+    onAdd: function(map) {
+        this._marker = L.marker(this.options.latlng);
+        this._circle = L.circle(this.options.latlng, this.options.accuracy);
+        map.addLayer(this._marker);
+        map.addLayer(this._circle);
+    },
+
+    onRemove: function(map) {
+        this._marker && map.removeLayer(this._marker);
+        this._circle && map.removeLayer(this._circle);
+        this._marker = null;
+        this._circle = null;
+    },
+
+    getBounds: function () {
+
+    }
 });
 
 nsGmx.LocationMarkerManager = L.Class.extend({
     options: {
-        maxZoom: 10
+        maxZoom: 14
     },
 
     initialize: function(options) {
@@ -18,10 +39,14 @@ nsGmx.LocationMarkerManager = L.Class.extend({
 
     render: function() {
         var latlng = this.options.locationModel.get('latlng');
+        var accuracy = this.options.locationModel.get('accuracy');
         if (this.options.locationModel.get('state') === 'success' && this._visible) {
             this.options.map.setView(latlng, this.options.maxZoom)
             if (!this._marker) {
-                this._marker = new nsGmx.LocationMarker(latlng);
+                this._marker = new nsGmx.LocationMarker({
+                    latlng: latlng,
+                    accuracy: accuracy
+                });
                 this.options.map.addLayer(this._marker);
             } else {
                 this._marker.setLatLng(latlng);
@@ -33,7 +58,7 @@ nsGmx.LocationMarkerManager = L.Class.extend({
     },
 
     showMarker: function() {
-        this.options.locationModel.ensureLocation().then(function () {
+        this.options.locationModel.ensureLocation().then(function() {
             this._visible = true;
             this.render();
         }.bind(this));
